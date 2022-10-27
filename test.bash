478ffdbdb126a7e52899e048648ff14bdbd791a3
yukihiro <s21c1059ya@s.chibakoudai.jp>
2022-10-27 12:23:59 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,6 +1,19 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Yukihiro Shiojima
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+      echo NG at Line $1
+      res=1
+}
+
+res=0
+
+### I/O TEST ###
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ] 
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res 
